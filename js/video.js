800cb9c2192dc8cac277d63b4ee1dafbc398cdb1
marlenabranham <143750608+marlenabranham@users.noreply.github.com>
2024-04-14 23:26:11 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -2,24 +2,15 @@ var video;
 var volume;
 
 window.addEventListener("load", function() {
-console.log("Good job opening the window")
-	
-video = document.querySelector("#player1")
-video.autoplay= false;
-video.loop= false;
-console.log('autoplay is set to '+ video.autoplay);
-console.log('loop is set to '+ video.loop);
-});
-	
-var video = document.querySelector("#player1");
-document.querySelector("#play").addEventListener("click", function() {
-console.log("Play Video")video.play();
-｝）；
-	
-var video = document.querySelector("#player1");
-document.querySelector("#pause").addEventListener("click", function() {
-console.log("Pause Video");
-video.pause( );
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1");
+	video.autoplay=false;
+	video.loop=false;
+	console.log('auto play is set to '+video.autoplay);
+	console.log('loop is set to '+video.loop);
+	volume = document.querySelector("#volume");
+	volume.innerHTML = video.volume * 100 + "%";
+
 });
 
 document.querySelector("#play").addEventListener("click", function() {
@@ -48,15 +39,15 @@ document.querySelector("#skip").addEventListener("click", function() {
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
 	if (video.muted) {
         video.muted = false;
-	document.querySelector("#mute").innerHTML= "Mute" ;
-	console.log("Unmute Video");
-    } 
-	else {
+		document.querySelector("#mute").innerHTML= "Mute"
+		console.log("Unmute");
+    } else {
         video.muted = true;
-	document.querySelector("#mute").innerHTML= "Unmute" ;
-	console.log("Mute Video");
+		document.querySelector("#mute").innerHTML= "Unmute"
+		console.log("Mute");
     }
 });
 document.querySelector("#slider").addEventListener("change", function() {
