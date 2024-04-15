4a7966e6c294d9a62c175e7c3702d926c4823d25
marlenabranham <143750608+marlenabranham@users.noreply.github.com>
2024-04-14 23:19:11 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,23 +1,25 @@
 var video;
 
 window.addEventListener("load", function() {
-console. log ("Good job opening the window")
-video = document. querySelector ("#player1" )
-video. autoplay = false;
-video. loop = false;
-console. log 'autoplay is set to '+ video.autoplay); console. log ('loop is set to '+ video. loop);
+console.log("Good job opening the window")
+	
+video = document.querySelector("#player1")
+video.autoplay= false;
+video.loop= false;
+console.log('autoplay is set to '+ video.autoplay);
+console.log('loop is set to '+ video.loop);
 });
 	
-var video = document. querySelector ("#player1");
-document. querySelector ("#play"). addEventListener("click", function () {
-console. log ("Play Video") video. play();
+var video = document.querySelector("#player1");
+document.querySelector("#play").addEventListener("click", function() {
+console.log("Play Video")video.play();
 ｝）；
 	
-var video = document. querySelector ("#player1");
-document querySelector ("#pause") addEventListener("click", function () {
-console. log ("Pause Video");
-video. pause ( );
-}) ;
+var video = document.querySelector("#player1");
+document.querySelector("#pause").addEventListener("click", function() {
+console.log("Pause Video");
+video.pause( );
+});
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
