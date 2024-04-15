aa59f32b8e73fb1287414d173a6fde9beb26ec44
marlenabranham <143750608+marlenabranham@users.noreply.github.com>
2024-04-14 23:12:15 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,17 +1,23 @@
 var video;
-var volume;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-	video = document.querySelector("#player1");
-	video.autoplay=false;
-	video.loop=false;
-	console.log('auto play is set to '+video.autoplay);
-	console.log('loop is set to '+video.loop);
-	volume = document.querySelector("#volume");
-	volume.innerHTML = video.volume * 100 + "%";
-
+console. log ("Good job opening the window")
+video = document. querySelector ("#player1" )
+video. autoplay = false;
+video. loop = false;
+console. log 'autoplay is set to '+ video.autoplay); console. log ('loop is set to '+ video. loop);
 });
+	
+var video = document. querySelector ("#player1");
+document. querySelector ("#play"). addEventListener("click", function () {
+console. log ("Play Video") video. play();
+｝）；
+	
+var video = document. querySelector ("#player1");
+document querySelector ("#pause") addEventListener("click", function () {
+console. log ("Pause Video");
+video. pause ( );
+}) ;
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
